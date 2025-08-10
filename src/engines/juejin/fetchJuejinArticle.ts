41fae30609166f82668aee5bde7da87c57ab8b6a
wtznicy <lebertesaysongkhamnkgo4602@gmail.com>
2025-08-10 15:03:40 +0800

    chore: update HTTP headers in `fetchJuejinArticle` to mimic iOS Safari for improved compatibility and modern standards

--- src/engines/juejin/fetchJuejinArticle.ts
+++ src/engines/juejin/fetchJuejinArticle.ts
@@ -7,19 +7,19 @@ export async function fetchJuejinArticle(url: string): Promise<{ content: string
 
         const response = await axios.get(url, {
             headers: {
-                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-                "Connection": "keep-alive",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Accept-Encoding": "gzip, deflate, br, zstd",
-                "pragma": "no-cache",
-                "cache-control": "no-cache",
-                "upgrade-insecure-requests": "1",
-                "sec-fetch-site": "none",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-user": "?1",
-                "sec-fetch-dest": "document",
-                "accept-language": "zh-CN,zh;q=0.9",
-                "priority": "u=0, i"
+                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+                'Connection': 'keep-alive',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'pragma': 'no-cache',
+                'cache-control': 'no-cache',
+                'upgrade-insecure-requests': '1',
+                'sec-fetch-site': 'none',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'accept-language': 'zh-CN,zh;q=0.9',
+                'priority': 'u=0, i'
             },
             timeout: 30000,
             decompress: true
