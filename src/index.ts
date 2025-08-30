e81feb2ae42510ca64032511ac2ec99ed744c7a4
wtznicy <lebertesaysongkhamnkgo4602@gmail.com>
2025-08-30 15:33:15 +0800

    chore: bump server version to 1.2.0 in configuration

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ async function main() {
   // Create MCP server
   const server = new McpServer({
     name: 'web-search',
-    version: '1.1.5'
+    version: '1.2.0'
   });
 
   // Set up server tools
